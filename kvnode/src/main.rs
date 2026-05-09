bc175723fcebf2d968fbf3851d8b3c157bff5440
Your Name <you@example.com>
2026-05-09 21:05:43 +0100

    push

--- kvnode/src/main.rs
+++ kvnode/src/main.rs
@@ -1,7 +1,12 @@
 use kvnode::node::Node;
+use tracing::{Level, event, info, span};
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt().init();
+
+    info!("init");
     let node = Node {};
     node.run().await;
     println!("Hello, world!");
--- kvnode/src/node.rs
+++ kvnode/src/node.rs
@@ -2,16 +2,21 @@
 
 use std::{
     collections::HashMap,
+    default,
     sync::{Arc, Mutex},
 };
 
 use axum::{
     Json, Router,
-    extract::{Path, State},
-    routing::get,
+    extract::{Path, Query, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{delete, get, post, put},
 };
 use serde::Serialize;
 use thiserror::Error;
+use tower_http::trace::TraceLayer;
+use tracing::{info, instrument};
 
 #[derive(Error, Debug, Serialize)]
 pub enum NodeError {
@@ -23,60 +28,115 @@ pub enum NodeError {
 struct StoredValue {
     key: String,
     value: Option<String>,
-    error: Option<NodeError>,
 }
 
 pub struct Node {}
 
-#[derive(Default)]
-struct AppState {
-    kv: HashMap<String, String>,
-}
-impl AppState {
-    pub fn get_val(&self, key: &String) -> anyhow::Result<String, NodeError> {
-        if let Some(val) = self.kv.get(key) {
-            return Ok(val.clone());
-        }
-        Err(NodeError::KeyDidNotExist(key.clone()))
-    }
-}
-
 impl Node {
-    // Run the node
-    pub async fn run(self) {
-        let shared_state = Arc::new(Mutex::new(AppState::default()));
+    fn router() -> Router {
+        let shared_state = Arc::new(Mutex::new(HashMap::new()));
         shared_state
             .lock()
             .unwrap()
-            .kv
             .insert("test".into(), "value".into());
-        let app = Router::new()
+        Router::new()
             .route("/key/{key}", get(Node::handle_get_val))
-            .with_state(shared_state);
+            .route("/key/{key}", put(Node::handle_put_val))
+            .route("/key/{key}", delete(Node::handle_del_val))
+            .with_state(shared_state)
+            .layer(TraceLayer::new_for_http())
+    }
+
+    // Run the node
+    pub async fn run(self) {
+        let app = Node::router();
         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
             .await
             .expect("Failed to bind port");
         axum::serve(listener, app).await;
     }
 
+    async fn handle_del_val(
+        State(state): State<Arc<Mutex<HashMap<String, String>>>>,
+        Path(key): Path<String>,
+    ) -> StatusCode {
+        state.lock().unwrap().remove(&key);
+        info!(message = "Deleted key", key = key);
+        StatusCode::OK
+    }
+
+    async fn handle_put_val(
+        State(state): State<Arc<Mutex<HashMap<String, String>>>>,
+        Path(key): Path<String>,
+        Query(params): Query<HashMap<String, String>>,
+    ) -> (StatusCode) {
+        if let Some(val) = params.get("value") {
+            state
+                .lock()
+                .unwrap()
+                .entry(key.clone())
+                .insert_entry(val.into());
+            info!(message = "Updated key", key = key, new_value = val);
+        } else {
+            return StatusCode::BAD_REQUEST;
+        }
+        (StatusCode::OK)
+    }
+
     // Get a value from the KV store
     async fn handle_get_val(
-        State(state): State<Arc<Mutex<AppState>>>,
+        State(state): State<Arc<Mutex<HashMap<String, String>>>>,
         Path(key): Path<String>,
-    ) -> Json<StoredValue> {
-        let val = state.lock().unwrap().get_val(&key);
-        if let Ok(val) = val {
-            Json(StoredValue {
-                key,
-                value: Some(val),
-                error: None,
-            })
+    ) -> (StatusCode, Json<StoredValue>) {
+        info!(message = "get key", key = key);
+        if let Some(val) = state.lock().unwrap().get(&key) {
+            (
+                StatusCode::OK,
+                Json(StoredValue {
+                    key,
+                    value: Some(val.clone()),
+                }),
+            )
         } else {
-            Json(StoredValue {
-                key,
-                value: None,
-                error: val.err(),
-            })
+            (
+                StatusCode::NOT_FOUND,
+                Json(StoredValue { key, value: None }),
+            )
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use axum_test::TestServer;
+    use serde_json::json;
+    #[tokio::test]
+    async fn test_ops() {
+        let router = Node::router();
+        let server = TestServer::new(router);
+        let response = server
+            .get("/key/unknown_key")
+            .await
+            .assert_status(StatusCode::NOT_FOUND);
+        let response = server
+            .put("/key/new_key?value=new_value")
+            .await
+            .assert_status(StatusCode::OK);
+        let response = server
+            .get("/key/new_key")
+            .await
+            .assert_status(StatusCode::OK)
+            .assert_json(&json!({"key": "new_key", "value": "new_value"}));
+
+        let response = server
+            .delete("/key/new_key")
+            .await
+            .assert_status(StatusCode::OK);
+        let response = server
+            .get("/key/new_key")
+            .await
+            .assert_status(StatusCode::NOT_FOUND);
     }
 }
