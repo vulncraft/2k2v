a39fe22d1601669f503c6a198e28ef765eff6489
iain <259941297+vulncraft@users.noreply.github.com>
2026-05-10 17:12:50 +0100

    add cli

--- kvnode/src/main.rs
+++ kvnode/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use tokio::sync::mpsc;
 
 use kvnode::{
@@ -6,14 +7,32 @@ use kvnode::{
 };
 use tracing::info;
 
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    #[arg(short, long)]
+    address: Option<String>,
+
+    #[arg(short, long)]
+    port: Option<u32>,
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
-
+    let args = Args::parse();
     info!("init");
+
     let (tx, rx) = mpsc::channel::<StoreCommand>(32);
     tokio::spawn(node_actor(rx));
-    let node = NodeHttp { store_tx: tx };
+    let node = NodeHttp {
+        store_tx: tx,
+        bind_address: format!(
+            "{}:{}",
+            args.address.unwrap_or("localhost".into()),
+            args.port.unwrap_or(3000)
+        ),
+    };
 
     node.run().await;
 }
